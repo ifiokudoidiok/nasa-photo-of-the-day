eba605fd3902fa63696b72b656ad0a2df5ec8cab
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-11 14:09:21 +0100

    Axios installed, API fetched

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState, useEffect}from "react";
+import axios from 'axios';
 import "./App.css";
 
+const nasaApi = 'https://api.nasa.gov/planetary/apod?api_key=YoeP5szYbSrnFTE4KUqS2aUbANxWq7flop3fbkBb';
 function App() {
+
+  useEffect(() => {
+    axios.get(nasaApi)
+      .then(response =>{
+        // debugger
+      })
+      .catch(error => {
+        
+      })
+  }, [])
   return (
     <div className="App">
       <p>
