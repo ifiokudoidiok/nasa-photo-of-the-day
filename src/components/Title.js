3551df52bdc6e444f872cbed43d4ac161b76b864
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-12 12:28:09 +0100

    Installed styled-components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from 'styled-components';
 
 const nasaApi =
   "https://api.nasa.gov/planetary/apod?api_key=YoeP5szYbSrnFTE4KUqS2aUbANxWq7flop3fbkBb";
